2ce1adfc9496e6c7d1f28f06f0ffc081d0074ef0
mohamedfayznada-jpg <mohamedfayznada@gmail.com>
2026-06-17 11:22:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -21,5 +21,42 @@ async function loadFactoryData() {
     });
 
 }
+async function loadFactoryData() {
+
+    const response =
+    await fetch("./data/factory_data.json");
+
+    const data =
+    await response.json();
+
+    const factoryMap =
+    document.querySelector(".factory-map");
+
+    data.zones.forEach(zone=>{
+
+        if(zone.x && zone.y){
+
+            const marker =
+            document.createElement("div");
+
+            marker.className =
+            "marker";
+
+            marker.innerText =
+            zone.name;
+
+            marker.style.left =
+            zone.x + "%";
+
+            marker.style.top =
+            zone.y + "%";
+
+            factoryMap.appendChild(marker);
+
+        }
+
+    });
+
+}
 
 loadFactoryData();
